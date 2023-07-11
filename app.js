3ffad9dcfb62f0b140984b7d72b0102b029fdf39
Kahuan <kahuanvitelli@gmail.com>
2023-07-10 21:14:12 -0300

    Adicionando Routes com GET, DELETE, POST E PATCH

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const rotaLivro = require("./rotas/livro")
+const rotaFavorito = require("./rotas/favorito")
+
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
-
-const port = 8000
+app.use(cors({origin: "*"}))
 
 app.use('/livros', rotaLivro)
+app.use('/favoritos', rotaFavorito)
 
-app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`)
-})
+const port = 8000
